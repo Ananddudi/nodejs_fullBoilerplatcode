c52dcc91c278ed2406bb8e8482a161abb83f07ca
anand dudi <dudis.vision@gmail.com>
2022-08-23 00:27:17 +0530

    Email capability inserted

---
+++ controllers/mailservice.js
@@ -0,0 +1,46 @@
+const nodemailer = require('nodemailer')
+
+
+const postmail = async(req,res)=>{
+	
+	const nodemailer = require('nodemailer');
+
+	const transporter = nodemailer.createTransport({
+	  service: 'gmail',
+	  auth: {
+	    type: 'OAuth2',
+	    user: process.env.EMAIL,
+	    pass: process.env.PASSWORD,
+	    clientId: process.env.CLIENT_ID,
+	    clientSecret: process.env.CLIENT_SECRET,
+	    refreshToken: process.env.REFRESH_TOKEN
+	  }
+	});
+
+	const mailConfigurations = {
+  
+    // It should be a string of sender email
+    from: 'dudianand@gmail.com',
+      
+    // Comma Separated list of mails
+    to: 'dudis.vision@gmail.com',
+  
+    // Subject of Email
+    subject: 'Sending Email using Node.js',
+      
+    // This would be the text of email body
+    text: 'Hi! There, You know I am using the'
+      + ' NodeJS Code along with NodeMailer '
+      + 'to send this email. yooooooooooo man'
+};
+
+transporter.sendMail(mailConfigurations, function(error, info){
+    if (error){
+    	return res.status(401).json({Error:error})
+    };
+    res.send(info)
+});
+
+}
+
+module.exports = postmail
--- routes/routes.js
+++ routes/routes.js
@@ -7,11 +7,13 @@ const {
 } = require('../controllers/getall')
 
 const {privateemp,createprivateemp} = require('../controllers/getempprivate')
+const postmail = require('../controllers/mailservice')
 const login = require('../controllers/login')
 const register = require('../controllers/register')
 
 
 router.route('/').get(getallemp).post(postemp)
+router.route('/getmail').get(postmail)
 router.route('/gets').get(privateemp).post(createprivateemp)	
 router.route('/login').post(login)
 router.route('/register').post(register)
